20c12c01598117f1cf1c9032dbfae761bea4d094
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-07-04 22:02:37 +0300

    Update controller RegisterController

--- src/main/java/com/uptalent/account/controller/RegisterController.java
+++ src/main/java/com/uptalent/account/controller/RegisterController.java
@@ -1,19 +1,24 @@
 package com.uptalent.account.controller;
 
-import com.uptalent.account.model.AuthRegister;
-import com.uptalent.account.model.AuthResponse;
+import com.uptalent.account.model.request.AuthRegister;
+import com.uptalent.account.model.response.RegisterResponse;
 import com.uptalent.account.service.RegisterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/account/save")
+@RequestMapping("/api/v1/account")
 public class RegisterController {
     private final RegisterService registerService;
 
-    @PostMapping
-    public AuthResponse save(@RequestBody AuthRegister authRegister) {
+    @PostMapping("/save")
+    public RegisterResponse save(@RequestBody AuthRegister authRegister) {
         return registerService.save(authRegister);
+    }
+
+    @GetMapping("/me")
+    public String get() {
+        return "authorized";
     }
 }
