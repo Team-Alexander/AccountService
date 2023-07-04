83f1951d65bd4d5f8e224e463aabbfe9a6617fc2
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-07-04 22:24:31 +0300

    Update security config and create test endpoint

--- src/main/java/com/uptalent/account/config/SecurityConfig.java
+++ src/main/java/com/uptalent/account/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.uptalent.account.config;
 
-import com.uptalent.account.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +9,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.POST;
 
@@ -20,7 +19,6 @@ import static org.springframework.http.HttpMethod.POST;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -28,7 +26,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authRequest ->
                         authRequest.requestMatchers(POST, "/api/v1/account/save").permitAll()
                                 .anyRequest().authenticated())
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
     }
 
--- src/main/java/com/uptalent/account/controller/RegisterController.java
+++ src/main/java/com/uptalent/account/controller/RegisterController.java
@@ -2,6 +2,7 @@ package com.uptalent.account.controller;
 
 import com.uptalent.account.model.request.AuthRegister;
 import com.uptalent.account.model.response.RegisterResponse;
+import com.uptalent.account.service.AccountService;
 import com.uptalent.account.service.RegisterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/account")
 public class RegisterController {
     private final RegisterService registerService;
+    private final AccountService accountService;
 
     @PostMapping("/save")
     public RegisterResponse save(@RequestBody AuthRegister authRegister) {
@@ -18,7 +20,7 @@ public class RegisterController {
     }
 
     @GetMapping("/me")
-    public String get() {
-        return "authorized";
+    public Long get() {
+        return accountService.getIdFromSubject();
     }
 }
