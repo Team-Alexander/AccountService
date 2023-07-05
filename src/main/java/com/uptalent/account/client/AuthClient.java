49b5e968b84bd4e1ac5eb033fd78ff6670e2f8cd
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-05 17:44:26 +0300

    Add retry mechanism to AuthClient

--- src/main/java/com/uptalent/account/client/AuthClient.java
+++ src/main/java/com/uptalent/account/client/AuthClient.java
@@ -1,10 +1,12 @@
 package com.uptalent.account.client;
 
 import com.uptalent.account.model.response.PublicKeyDTO;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient("auth-service")
+@Retry(name = "default")
 public interface AuthClient {
     @GetMapping("/api/v1/auth/public-key")
     PublicKeyDTO getPublicKey();
