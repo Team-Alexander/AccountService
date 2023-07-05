d509ad1d557365642434f581027cda774f809598
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-05 17:41:52 +0300

    Rename RegisterConstant to AccountConstant

--- src/main/java/com/uptalent/account/model/constant/AccountConstant.java
+++ src/main/java/com/uptalent/account/model/constant/AccountConstant.java
@@ -1,5 +1,5 @@
 package com.uptalent.account.model.constant;
 
-public class RegisterConstant {
+public class AccountConstant {
     public static final String ACCOUNT_NOT_FOUND_ERROR = "Account not found by email [%s]";
 }
