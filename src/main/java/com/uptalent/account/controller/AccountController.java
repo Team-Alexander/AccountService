2a6e731576f1aefab06cd0e64c4981398a945123
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-13 16:26:30 +0300

    Remove AccountSecurityService and refine save endpoint

--- src/main/java/com/uptalent/account/controller/AccountController.java
+++ src/main/java/com/uptalent/account/controller/AccountController.java
@@ -2,9 +2,9 @@ package com.uptalent.account.controller;
 
 import com.uptalent.account.model.request.AuthRegister;
 import com.uptalent.account.model.response.AuthResponse;
-import com.uptalent.account.service.AccountSecurityService;
 import com.uptalent.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,15 +12,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/account")
 public class AccountController {
     private final AccountService accountService;
-    private final AccountSecurityService accountSecurityService;
 
     @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
     public AuthResponse save(@RequestBody AuthRegister authRegister) {
         return accountService.save(authRegister);
-    }
-
-    @GetMapping("/me")
-    public Long get() {
-        return accountSecurityService.getIdFromPrincipal();
     }
 }
