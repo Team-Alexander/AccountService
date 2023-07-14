2b6ce784730a72045d1edd6a498cedc84b8596c6
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-07-14 15:00:37 +0300

    Create endpoint and method for login user

--- src/main/java/com/uptalent/account/controller/AccountController.java
+++ src/main/java/com/uptalent/account/controller/AccountController.java
@@ -1,5 +1,6 @@
 package com.uptalent.account.controller;
 
+import com.uptalent.account.model.request.AuthLogin;
 import com.uptalent.account.model.request.AuthRegister;
 import com.uptalent.account.model.response.AuthResponse;
 import com.uptalent.account.service.AccountService;
@@ -17,5 +18,11 @@ public class AccountController {
     @ResponseStatus(HttpStatus.CREATED)
     public AuthResponse save(@RequestBody AuthRegister authRegister) {
         return accountService.save(authRegister);
+    }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public AuthResponse login(@RequestBody AuthLogin authLogin) {
+        return accountService.login(authLogin);
     }
 }
