a12496df6277531fa1221c86d1c62bfb311e914b
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-07-14 12:16:04 +0300

    Add crud operations in API /api/v1/account/sponsors

--- src/main/java/com/uptalent/account/controller/SponsorController.java
+++ src/main/java/com/uptalent/account/controller/SponsorController.java
@@ -3,8 +3,10 @@ package com.uptalent.account.controller;
 import com.uptalent.account.model.request.SponsorUpdate;
 import com.uptalent.account.model.response.AccountProfile;
 import com.uptalent.account.service.AccountService;
+import com.uptalent.account.service.SponsorService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +15,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/account/sponsors")
 public class SponsorController {
     private final AccountService accountService;
+    private final SponsorService sponsorService;
 
     @GetMapping("/{id}")
     @PreAuthorize("#id == authentication.principal and hasAuthority('SPONSOR')")
     public AccountProfile getSponsorProfile(@PathVariable Long id){
-        //TODO implement
-        return null;
+        return sponsorService.getSponsorProfile(id);
     }
 
     @PatchMapping("/{id}")
@@ -26,5 +28,12 @@ public class SponsorController {
     public AccountProfile updateSponsor(@PathVariable Long id,
                                         @RequestBody @Valid SponsorUpdate sponsorUpdate){
         return accountService.updateProfile(id, sponsorUpdate);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("#id == authentication.principal and hasAuthority('SPONSOR')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTalent(@PathVariable Long id){
+        accountService.deleteProfile(id);
     }
 }
