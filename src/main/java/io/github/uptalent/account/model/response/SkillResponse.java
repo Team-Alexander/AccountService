c6f95b9f9b8e095c3d99be61f7058d39a5561528
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-09-13 16:15:40 +0300

    Added Builder annotation to SkillResponse model

--- src/main/java/io/github/uptalent/account/model/response/SkillResponse.java
+++ src/main/java/io/github/uptalent/account/model/response/SkillResponse.java
@@ -1,9 +1,11 @@
 package io.github.uptalent.account.model.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class SkillResponse {
     private long id;
